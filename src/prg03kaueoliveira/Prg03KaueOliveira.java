131257bb29d2b766f530d76d002392dd022636e4
ETM-00168 <ETM-00168@ETM-00168.home>
2025-10-12 14:22:01 -0300

    atividade 07, criando classe

--- src/prg03kaueoliveira/Prg03KaueOliveira.java
+++ src/prg03kaueoliveira/Prg03KaueOliveira.java
@@ -4,6 +4,10 @@
  */
 package prg03kaueoliveira;
 
+import br.com.ifba.atividade07.ContaBanco;
+
+
+
 /**
  *
  * @author ETM-00168
@@ -15,6 +19,25 @@ public class Prg03KaueOliveira {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       ContaBanco cliente01 = new ContaBanco();
+       cliente01.numConta = 181795;
+          
+       
+       cliente01.abrirConta("cc", "kaue oliveira");
+       cliente01.depositarConta(100);
+       cliente01.sacarConta(20);
+       cliente01.fecharConta();
+       cliente01.statusAtual();
+       
+       ContaBanco cliente02 = new ContaBanco();
+       cliente02.numConta = 3597461;
+       
+       cliente02.abrirConta("cp", "kelly patricia");
+       cliente02.depositarConta(200);
+       cliente02.sacarConta(350);
+       cliente02.fecharConta();
+       cliente02.statusAtual();
+       
     }
     
 }
